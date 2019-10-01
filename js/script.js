63f96adc2e352b3235a8a4944abd34f2fbcd57d3
Krzysiek <krzysiek@popracy.eu>
2019-10-01 14:13:12 +0200

    map add

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ var elem = document.querySelector('.main-carousel');
 var progressBar = document.querySelector('.progress-bar');
 var buttonClickReset = document.querySelector('.button');
 
+var templateSlide = document.getElementById('template-slide').innerHTML;
+
+for (var i = 0; i < slajds.length; i++) {
+var generatedSlide = Mustache.render(templateSlide, slajds[i]);
+
+result.insertAdjacentHTML('beforeend', generatedSlide);
+};
+
 var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
@@ -18,3 +26,15 @@ flkty.on('scroll', function( progress ) {
 buttonClickReset.addEventListener('click', function() {
   flkty.select(0);
 });
+
+// Initialize and add the map
+window.initMap = function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 49.628167, lng: 20.703000};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
