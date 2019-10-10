e101b283b122fba75ba0f5cc58046f55c30511bd
Krzysiek <krzysiek@popracy.eu>
2019-10-10 21:16:53 +0200

    code clean

--- js/script.js
+++ js/script.js
@@ -4,16 +4,12 @@ var buttonClickReset = document.querySelector('.button');
 
 var templateSlide = document.getElementById('template-slide').innerHTML;
 
-
-
 for (var i = 0; i < slajds.length; i++) {
-var generatedSlide = Mustache.render(templateSlide, slajds[i]);
-
-result.insertAdjacentHTML('beforeend', generatedSlide);
+  var generatedSlide = Mustache.render(templateSlide, slajds[i]);
+  result.insertAdjacentHTML('beforeend', generatedSlide);
 };
 
 var flkty = new Flickity(elem, {
-  // options
   cellAlign: 'left',
   contain: true,
   hash: true,
@@ -25,35 +21,23 @@ var flkty = new Flickity(elem, {
 flkty.on('scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
-  
 });
-
 
 buttonClickReset.addEventListener('click', function() {
   flkty.select(0);
 });
 
-  // Initialize and add the map
 window.initMap = function initMap() {
-  // The location of Uluru
-  //var uluru = {lat: 49.628167, lng: 20.703000};
-
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: slajds[0].coords});
-  // The marker, positioned at Uluru
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: slajds[0].coords});
   var flkty = new Flickity('.main-carousel');
-flkty.on( 'change', function( index ) {
-  map.panTo(slajds[index].coords);
+  flkty.on( 'change', function( index ) {
+    map.panTo(slajds[index].coords);
 });
-
   for (let i = 0; i < slajds.length; i++) {
-    var marker = new google.maps.Marker({position: slajds[i].coords, map: map}); 
-    
+    var marker = new google.maps.Marker({position: slajds[i].coords, map: map});   
     marker.addListener('click', function() {
       flkty.select(i)
     });
-  
   }
 }
 
